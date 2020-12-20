9e91ae4c93211b6aaaa00d3ed5e0a466671d2a42
USER <USER@sfu.ca>
2020-12-20 13:50:18 -0800

    re-add gunicorn

--- app.py
+++ app.py
@@ -58,4 +58,6 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    # app.run(host="localhost", port=5000, debug=True) #only for testing
+
+    app.run(threaded=True, port=5000)
