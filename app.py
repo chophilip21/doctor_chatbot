da524fc906cc6a5e1a5b499cc5fa2d68b13ed55d
Philip Cho <chophilip21@gmail.com>
2020-12-20 23:40:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,55 +3,78 @@ import requests
 import json
 import os
 import emoji
+import re
+from static.macro import PROFANITY, GREETINGS, FAREWELL, MODEL_ARGS
+import pandas as pd
+import COVID19Py
+import random
+from simpletransformers.seq2seq import Seq2SeqModel
+
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-#The first index page
+# The first index page
 @app.route("/", methods=['GET', 'POST'])
 def chat():
     return render_template('chatroom.html')
 
+
 """
-TODO: 
+TODO:
 - When the bot responds Take care and bye! The session is done. You should no longer be able to type
 - Replace the type message to start another session, or logout(refresh..? clear output..?), whatever.
-- Add grammar checker. There will be a lot of grammar problems for sure. 
+- Add grammar checker. There will be a lot of grammar problems for sure.
 """
 
+
 @app.route("/get")
-def get_bot_response():    
+def get_bot_response():
     userText = request.args.get('msg')
-  
-    #TODO: You will instantiate the model here
-    # bot = Seq2SeqModel()
-    # response = bot.predict(userText)
-    # response = ' '.join(response) 
-    
-    greetings_1 = "hello"
-    greetings_2 = "Hi there"
-    greetings_3 = "Hi"
-    greetings_4 = "Hello"
-    greetings_5 = "hi"
-    greetings = [greetings_1, greetings_2, greetings_3, greetings_4, greetings_5]
 
-    farewell_1 = 'bye'
-    farewell_2 = "okay bye"
-    farewell_3 = 'see you'
-    farewell_4 = 'thanks bye'
-    farewell_5 = "Bye"
-    farewell = [farewell_1, farewell_2, farewell_3, farewell_4, farewell_5]
+    response = "I am unable to answer your question at the moment. Please contact emergency if this is an urgent matter."
 
-    response = "Sorry Me completely untrained right now. Too dumb to answer your qestion"
-    
-    if len(userText) <10 and any(map(userText.startswith, greetings)):
-        response = "Hi there, how can I help you today?"
-      
-    if len(userText) <10 and any(map(userText.startswith, farewell)):
-        response = "Take care and bye!"
-    
+    if len(userText) <= 1 or len(userText) >= 35:
+        response = 'Sorry about that. Your question is not valid in terms of length. Try rewording it so that I can understand'
+
+    else:
+
+        # default Macro
+        if userText.startswith('stats:covid'):
+            covid19 = COVID19Py.COVID19(data_source="csbs")
+
+            stats = covid19.getLatest()
+
+            confirmed = stats['confirmed']
+            deaths = stats['deaths']
+
+            response = f'Here are the quick stats. \n  Total confirmed: {confirmed}, \n Total deaths: {deaths}'
+
+        elif re.search("("+")|(".join(PROFANITY)+")", userText):
+            response = 'I am sorry but I really do not want to hear any hate speach from you. Please ask a valid question, or leave'
+
+        elif len(userText) < 10 and any(map(userText.startswith, GREETINGS)):
+            response = ["Hi there, how can I help you today?",
+                        'Hey, how can I help?', 'How may I help you today?', "Hello there!", "Hi, feel free to ask any questions", "How can I help you today?"]
+            response = random.choice(response)
+
+        elif len(userText) < 10 and any(map(userText.startswith, FAREWELL)):
+            response = ["Take care and bye!", "Okay bye!",
+                        "Thanks for visiting", "Okay stay safe!", "Goodbye!", "Farewell!"]
+            response = random.choice(response)
+
+        else:
+            #non macro
+            model = Seq2SeqModel(
+                "distilbert",
+                encoder_decoder_name="best_model/",
+                args=MODEL_ARGS)
+
+            botresponse = model.predict([userText])
+            response = ' '.join(botresponse)
+
     response = emoji.emojize(':pill: {}'.format(response))
 
     return response
